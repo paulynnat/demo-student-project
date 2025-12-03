1bf6927d86bea5acb86fdd3a82eb892b792ae34d
unknown <paulynnat@gmail.com>
2025-12-03 14:51:16 +0800

    Update Vision Board: tooltips and park image

--- modules/vision-board/script.js
+++ modules/vision-board/script.js
@@ -1,5 +1,16 @@
 document.querySelectorAll('.hover-item').forEach(item => {
-  item.addEventListener('mouseover', () => {
-    alert(item.dataset.story);
+  item.addEventListener('mouseenter', function() {
+    const tooltip = document.createElement('div');
+    tooltip.className = 'tooltip';
+    tooltip.innerText = this.dataset.story;
+    document.body.appendChild(tooltip);
+
+    const rect = this.getBoundingClientRect();
+    tooltip.style.left = rect.left + window.scrollX + 'px';
+    tooltip.style.top = rect.bottom + window.scrollY + 'px';
+
+    this.addEventListener('mouseleave', () => {
+      tooltip.remove();
+    }, { once: true });
   });
 });
